c40bad575d83d80d8ba92c8df1683ce5c6de32e2
Luiz-Trindade <luiz.gabriel.m.trindade@gmail.com>
2025-05-18 13:06:28 -0300

    feat: improved main.go

--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"fmt"
     "net/http"
     "github.com/gorilla/mux"
 )
 
 func main() {
-    router := mux.NewRouter()
+    router 		:= mux.NewRouter()
+    serverPort 	:= 8080
+
     router.HandleFunc("/", func(w http.ResponseWriter, router *http.Request) {
-        w.Write([]byte("Servidor Go rodando! ✅"))
+        w.Write([]byte("Servidor Go rodando! ✅ \n"))
     })
-    http.ListenAndServe(":8080", router)
+    
+    fmt.Printf("Servidor rodando na porta :%d\n", serverPort)
+    
+	addr := fmt.Sprintf(":%d", serverPort)
+    http.ListenAndServe(addr, router)
 }
